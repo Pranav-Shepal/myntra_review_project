032abaf4a4fe226b8384bebbafa32e99893536ef
Pranav Shepal <pranavshepal07@gmail.com>
2024-11-17 12:06:10 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='myntra scrapper',
+    version='0.0.1',
+    author='pranav',
+    author_email='pranavshepal07@gmail.com',
+    packages=find_packages(),
+    install_requirements=[]
+)
